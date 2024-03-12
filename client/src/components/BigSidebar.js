004c1402bb1d51b6b5996e8af49b5f690f20c3bd
TimNet <jesulayomitimze55@gmail.com>
2024-03-12 15:10:10 +0100

    first commit

---
+++ client/src/components/BigSidebar.js
@@ -0,0 +1,29 @@
+import NavLinks from './NavLinks';
+import Logo from '../components/Logo';
+import Wrapper from '../assets/wrappers/BigSidebar';
+import { useSelector } from 'react-redux';
+
+const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen
+            ? 'sidebar-container '
+            : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+      <h1>Timothy </h1>
+    </Wrapper>
+  );
+};
+export default BigSidebar;
